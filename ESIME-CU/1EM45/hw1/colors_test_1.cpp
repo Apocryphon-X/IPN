4b9c7921ea811c7e167b7c9c3b482524939a22ed
Dante Mendoza Leyva <apocryphon.x.contact@gmail.com>
2022-02-05 20:54:31 -0600

    Update colors_test_1.cpp

--- ESIME-CU/1EM45/hw1/colors_test_1.cpp
+++ ESIME-CU/1EM45/hw1/colors_test_1.cpp
@@ -20,76 +20,13 @@ int main()
 {
     system(""); // Hack to enable VT Mode on Windows CMD via 'cmd.exe /c ...'
 
-    cout << "* Standard background colors test:         [";
-
-    cout << COLOR.BG_BLACK   << ' ';
-    cout << COLOR.BG_RED     << ' ';
-    cout << COLOR.BG_GREEN   << ' ';
-    cout << COLOR.BG_YELLOW  << ' ';
-    cout << COLOR.BG_BLUE    << ' ';
-    cout << COLOR.BG_MAGENTA << ' ';
-    cout << COLOR.BG_CYAN    << ' ';
-    cout << COLOR.BG_WHITE   << ' ';
-    cout << COLOR.NORMAL << "]\n";
-
-    cout << "* Standard background bright colors test:  [";
-
-    cout << COLOR.BG_B_BLACK   << ' ';
-    cout << COLOR.BG_B_RED     << ' ';
-    cout << COLOR.BG_B_GREEN   << ' ';
-    cout << COLOR.BG_B_YELLOW  << ' ';
-    cout << COLOR.BG_B_BLUE    << ' ';
-    cout << COLOR.BG_B_MAGENTA << ' ';
-    cout << COLOR.BG_B_CYAN    << ' ';
-    cout << COLOR.BG_B_WHITE   << ' ';
-    cout << COLOR.NORMAL << "]\n\n";
-
-    cout << "* Standard foreground colors test:         [";
-
-    cout << COLOR.FG_BLACK   << 'a';
-    cout << COLOR.FG_RED     << 'b';
-    cout << COLOR.FG_GREEN   << 'c';
-    cout << COLOR.FG_YELLOW  << 'd';
-    cout << COLOR.FG_BLUE    << 'e';
-    cout << COLOR.FG_MAGENTA << 'f';
-    cout << COLOR.FG_CYAN    << 'g';
-    cout << COLOR.FG_WHITE   << 'h';
-    cout << COLOR.NORMAL << "]\n";
-
-    cout << "* Standard foreground bright colors test:  [";
-
-    cout << COLOR.FG_B_BLACK   << 'a';
-    cout << COLOR.FG_B_RED     << 'b';
-    cout << COLOR.FG_B_GREEN   << 'c';
-    cout << COLOR.FG_B_YELLOW  << 'd';
-    cout << COLOR.FG_B_BLUE    << 'e';
-    cout << COLOR.FG_B_MAGENTA << 'f';
-    cout << COLOR.FG_B_CYAN    << 'g';
-    cout << COLOR.FG_B_WHITE   << 'h';
-    cout << COLOR.NORMAL << "]\n\n";
-
-    cout << "* RGB foreground colors test:              [ ";
-
     cout << COLOR.FG_RGB(66, 133, 244) << 'G';
     cout << COLOR.FG_RGB(234, 67, 53)  << 'o';
     cout << COLOR.FG_RGB(251, 188, 5)  << 'o';
     cout << COLOR.FG_RGB(66, 133, 244) << 'g';
     cout << COLOR.FG_RGB(52, 168, 83)  << 'l';
     cout << COLOR.FG_RGB(234, 67, 53)  << 'e';
-    cout << COLOR.NORMAL << " ]\n";
-
-    cout << "* RGB background colors test:              [ ";
-    cout << COLOR.FG_RGB(255, 255, 255);
-
-    cout << COLOR.BG_RGB(66, 133, 244) << 'G';
-    cout << COLOR.BG_RGB(234, 67, 53)  << 'o';
-    cout << COLOR.BG_RGB(251, 188, 5)  << 'o';
-    cout << COLOR.BG_RGB(66, 133, 244) << 'g';
-    cout << COLOR.BG_RGB(52, 168, 83)  << 'l';
-    cout << COLOR.BG_RGB(234, 67, 53)  << 'e';
-
-    cout << COLOR.NORMAL;
-    cout << " ]\n";
+    cout << COLOR.NORMAL << '\n';
 
     return 0;
 }
