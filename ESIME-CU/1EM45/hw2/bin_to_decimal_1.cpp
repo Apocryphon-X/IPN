d8b43642d6119a0da5e7bdf8d6e8eefbd0521d80
Dante Mendoza Leyva <apocryphon.x.contact@gmail.com>
2022-02-06 21:30:19 -0600

    Add 'ESIME-CU/1EM45/hw2/bin_to_decimal_1.cpp'

---
+++ ESIME-CU/1EM45/hw2/bin_to_decimal_1.cpp
@@ -0,0 +1,26 @@
+/**
+ *   Name: Dante Mendoza Leyva (1EM45)
+ *   Date:  06 / February / 2022
+**/
+
+#include <iostream>
+using namespace std;
+using i64 = long long;
+
+i64 Number;
+
+int main()
+{
+    cout << "(Decimal -> Binary) Number to convert: ";
+    cin >> Number;
+
+    cout << "(Conversion result) ";
+    for (int c = 63; c >= 0; c--)
+    {
+        cout << !!(Number & (1LL << c));
+        if (c % 8 == 0)
+            cout << ' ';
+    }
+
+    return 0;
+}
