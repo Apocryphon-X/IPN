d54af62571bc430b07be5c39ae75a6374f28a529
Dante Mendoza Leyva <apocryphon.x.contact@gmail.com>
2022-05-04 20:20:42 -0500

    Update easy_colors_2.hpp

--- 1EM45/include/easy_colors_2.hpp
+++ 1EM45/include/easy_colors_2.hpp
@@ -5,44 +5,44 @@
 
 namespace easy_colors
 {
-    const std::string fg_black   = "\x1b[30m";
-    const std::string fg_red     = "\x1b[31m";
-    const std::string fg_green   = "\x1b[32m";
-    const std::string fg_yellow  = "\x1b[33m";
-    const std::string fg_blue    = "\x1b[34m";
-    const std::string fg_magenta = "\x1b[35m";
-    const std::string fg_cyan    = "\x1b[36m";
-    const std::string fg_white   = "\x1b[37m";
+    const std::string black   = "\x1b[30m";
+    const std::string red     = "\x1b[31m";
+    const std::string green   = "\x1b[32m";
+    const std::string yellow  = "\x1b[33m";
+    const std::string blue    = "\x1b[34m";
+    const std::string magenta = "\x1b[35m";
+    const std::string cyan    = "\x1b[36m";
+    const std::string white   = "\x1b[37m";
 
-    const std::string fg_b_black   = "\x1b[90m";
-    const std::string fg_b_red     = "\x1b[91m";
-    const std::string fg_b_green   = "\x1b[92m";
-    const std::string fg_b_yellow  = "\x1b[93m";
-    const std::string fg_b_blue    = "\x1b[94m";
-    const std::string fg_b_magenta = "\x1b[95m";
-    const std::string fg_b_cyan    = "\x1b[96m";
-    const std::string fg_b_white   = "\x1b[97m";
+    const std::string b_black   = "\x1b[90m";
+    const std::string b_red     = "\x1b[91m";
+    const std::string b_green   = "\x1b[92m";
+    const std::string b_yellow  = "\x1b[93m";
+    const std::string b_blue    = "\x1b[94m";
+    const std::string b_magenta = "\x1b[95m";
+    const std::string b_cyan    = "\x1b[96m";
+    const std::string b_white   = "\x1b[97m";
 
-    const std::string bg_black   = "\x1b[40m";
-    const std::string bg_red     = "\x1b[41m";
-    const std::string bg_green   = "\x1b[42m";
-    const std::string bg_yellow  = "\x1b[43m";
-    const std::string bg_blue    = "\x1b[44m";
-    const std::string bg_magenta = "\x1b[45m";
-    const std::string bg_cyan    = "\x1b[46m";
-    const std::string bg_white   = "\x1b[47m";
+    const std::string on_black   = "\x1b[40m";
+    const std::string on_red     = "\x1b[41m";
+    const std::string on_green   = "\x1b[42m";
+    const std::string on_yellow  = "\x1b[43m";
+    const std::string on_blue    = "\x1b[44m";
+    const std::string on_magenta = "\x1b[45m";
+    const std::string on_cyan    = "\x1b[46m";
+    const std::string on_white   = "\x1b[47m";
 
-    const std::string bg_b_black   = "\x1b[100m";
-    const std::string bg_b_red     = "\x1b[101m";
-    const std::string bg_b_green   = "\x1b[102m";
-    const std::string bg_b_yellow  = "\x1b[103m";
-    const std::string bg_b_blue    = "\x1b[104m";
-    const std::string bg_b_magenta = "\x1b[105m";
-    const std::string bg_b_cyan    = "\x1b[106m";
-    const std::string bg_b_white   = "\x1b[107m";
+    const std::string on_b_black   = "\x1b[100m";
+    const std::string on_b_red     = "\x1b[101m";
+    const std::string on_b_green   = "\x1b[102m";
+    const std::string on_b_yellow  = "\x1b[103m";
+    const std::string on_b_blue    = "\x1b[104m";
+    const std::string on_b_magenta = "\x1b[105m";
+    const std::string on_b_cyan    = "\x1b[106m";
+    const std::string on_b_white   = "\x1b[107m";
 
     const std::string normal = "\x1b[0m";
-    
+
     const std::string bold      = "\x1b[1m";
     const std::string dim       = "\x1b[2m";
     const std::string italic    = "\x1b[3m";
@@ -52,14 +52,14 @@ namespace easy_colors
     const std::string conceal   = "\x1b[8m";
     const std::string strike    = "\x1b[9m";
 
-    std::string fg_rgb(short r, short g, short b)
+    std::string rgb(short r, short g, short b)
     {
         std::string rgb_esc_sequence("\x1b[38;2;");
         rgb_esc_sequence += std::to_string(r) + ";" + std::to_string(g) + ";" + std::to_string(b) + 'm';
         return rgb_esc_sequence;
     }
 
-    std::string bg_rgb(short r, short g, short b)
+    std::string on_rgb(short r, short g, short b)
     {
         std::string rgb_esc_sequence("\x1b[48;2;");
         rgb_esc_sequence += std::to_string(r) + ";" + std::to_string(g) + ";" + std::to_string(b) + 'm';
