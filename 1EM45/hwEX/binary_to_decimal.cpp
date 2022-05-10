6d4ecedac779a2e5955efcbe7a103c0db18930ad
Dante Mendoza Leyva <apocryphon.x.contact@gmail.com>
2022-05-09 19:48:41 -0500

    Renaming `bin_to_decimal_1.cpp`

---
+++ 1EM45/hwEX/binary_to_decimal.cpp
@@ -0,0 +1,30 @@
+/**
+ *   Name: Dante Mendoza Leyva (1EM45)
+ *   Date:  06 / February / 2022
+**/
+
+#include <iostream>
+using namespace std;
+using i64 = long long;
+
+i64 Number;
+
+int main()
+{
+    cout << "(Decimal -> Binary) Number to convert: ";
+    cin >> Number;
+    cin.ignore();
+
+    cout << "(Conversion result) ";
+    for (int c = 63; c >= 0; c--)
+    {
+        cout << !!(Number & (1LL << c));
+        if (c % 8 == 0)
+            cout << ' ';
+    }
+
+    cout << endl;
+    cout << "Press ENTER to continue.";
+    cin.get();
+    return 0;
+}
